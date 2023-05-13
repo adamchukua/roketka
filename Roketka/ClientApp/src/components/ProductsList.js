3368902a4c014c8c43c28da211aa35cbc20a835d
thegradle <adamchuk.v.1@gmail.com>
2023-05-13 23:00:39 +0300

    Fix bug (spin isn't centered)

--- Roketka/ClientApp/src/components/ProductsList.js
+++ Roketka/ClientApp/src/components/ProductsList.js
@@ -27,85 +27,87 @@ export default function ProductsList(props) {
 
     return (
         <PrintData status={products.status} error={products.error}>
-            <Row justify="space-between" style={{ marginBottom: 20 }}>
-                <Col>
-                    <Typography.Text type="secondary">Знайдено елементів: {products.products.length}</Typography.Text>
-                </Col>
+            <>
+                <Row justify="space-between" style={{ marginBottom: 20 }}>
+                    <Col>
+                        <Typography.Text type="secondary">Знайдено елементів: {products.products.length}</Typography.Text>
+                    </Col>
 
-                <Col>
-                    <Space>
-                        <Select
-                            defaultValue="Сортування"
-                            style={{ width: 220 }}
-                            onChange={(value) => dispatch(sortProductsByPrice(value))}
-                            options={[
-                                { value: 'cheap-to-expensive', label: 'Від дешевих до дорогих' },
-                                { value: 'expensive-to-cheap', label: 'Від дорогих до дешевих' },
-                            ]}
-                        />
+                    <Col>
+                        <Space>
+                            <Select
+                                defaultValue="Сортування"
+                                style={{ width: 220 }}
+                                onChange={(value) => dispatch(sortProductsByPrice(value))}
+                                options={[
+                                    { value: 'cheap-to-expensive', label: 'Від дешевих до дорогих' },
+                                    { value: 'expensive-to-cheap', label: 'Від дорогих до дешевих' },
+                                ]}
+                            />
 
-                        <Segmented
-                            options={[
-                                {
-                                    label: 'Картки',
-                                    value: 'Card',
-                                    icon: <AppstoreOutlined style={{ position: 'relative', top: '-3px' }} />,
-                                },
-                                {
-                                    label: 'Список',
-                                    value: 'List',
-                                    icon: <BarsOutlined style={{ position: 'relative', top: '-3px' }} />,
-                                },
-                            ]}
-                            onChange={handleSegmentedChange}
-                        />
-                    </Space>
-                </Col>
-            </Row>
-
-            {isCardView ? (
-                <Row gutter={[16, 16]}>
-                    {products.products
-                        .slice((currentPage - 1) * pageSize, currentPage * pageSize)
-                        .map((product) => (
-                            <Col span={props.spanCard || 6} key={product.id}>
-                                <a href={"/product/" + product.id}>
-                                    <Card
-                                        hoverable
-                                        style={{ width: 340 }}
-                                        cover={
-                                            (product.images.length > 0) && (<img
-                                                alt={product.title}
-                                                src={"images/" + product.id + "/" + product.images[0].path} />)
-                                        }
-                                    >
-                                        <Card.Meta title={product.title} description={product.price + currencyName} />
-                                    </Card>
-                                </a>
-                            </Col>
-                        ))}
+                            <Segmented
+                                options={[
+                                    {
+                                        label: 'Картки',
+                                        value: 'Card',
+                                        icon: <AppstoreOutlined style={{ position: 'relative', top: '-3px' }} />,
+                                    },
+                                    {
+                                        label: 'Список',
+                                        value: 'List',
+                                        icon: <BarsOutlined style={{ position: 'relative', top: '-3px' }} />,
+                                    },
+                                ]}
+                                onChange={handleSegmentedChange}
+                            />
+                        </Space>
+                    </Col>
                 </Row>
-            ) : (
-                <List
-                    dataSource={products.products
-                        .slice((currentPage - 1) * pageSize, currentPage * pageSize)}
-                    renderItem={(product) => (
-                        <List.Item key={product.id}>
-                            <List.Item.Meta title={<a href={"/product/" + product.id}>{product.title}</a>} description={product.price + currencyName} />
-                        </List.Item>
-                    )}
-                />
-            )}
 
-            <Row justify="center">
-                <Pagination
-                    current={currentPage}
-                    pageSize={pageSize}
-                    total={products.products.length}
-                    onChange={handlePageChange}
-                    style={{ marginTop: 20 }}
-                />
-            </Row>
+                {isCardView ? (
+                    <Row gutter={[16, 16]}>
+                        {products.products
+                            .slice((currentPage - 1) * pageSize, currentPage * pageSize)
+                            .map((product) => (
+                                <Col span={props.spanCard || 6} key={product.id}>
+                                    <a href={"/product/" + product.id}>
+                                        <Card
+                                            hoverable
+                                            style={{ width: 340 }}
+                                            cover={
+                                                (product.images.length > 0) && (<img
+                                                    alt={product.title}
+                                                    src={"images/" + product.id + "/" + product.images[0].path} />)
+                                            }
+                                        >
+                                            <Card.Meta title={product.title} description={product.price + currencyName} />
+                                        </Card>
+                                    </a>
+                                </Col>
+                            ))}
+                    </Row>
+                ) : (
+                    <List
+                        dataSource={products.products
+                            .slice((currentPage - 1) * pageSize, currentPage * pageSize)}
+                        renderItem={(product) => (
+                            <List.Item key={product.id}>
+                                <List.Item.Meta title={<a href={"/product/" + product.id}>{product.title}</a>} description={product.price + currencyName} />
+                            </List.Item>
+                        )}
+                    />
+                )}
+
+                <Row justify="center">
+                    <Pagination
+                        current={currentPage}
+                        pageSize={pageSize}
+                        total={products.products.length}
+                        onChange={handlePageChange}
+                        style={{ marginTop: 20 }}
+                    />
+                </Row>
+            </>
         </PrintData>
     )
 }
